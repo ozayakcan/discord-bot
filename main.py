cbda14e089c13f1e6ecbc233b06bd08b02648223
Özay Akcan <mail@ozayakcan.com.tr>
2023-03-18 22:30:52 +0000

    Added discord integration

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import os
 from keep_alive import keep_alive
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
 keep_alive()
+token = os.environ.get("DISCORD_BOT_SECRET") 
+client.run(token)
