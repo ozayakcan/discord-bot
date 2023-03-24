0de0034b25e88b35b4d434e91a94d86db2846c64
Özay Akcan <mail@ozayakcan.com.tr>
2023-03-24 05:50:44 +0000

    Added typing in ai message

--- src/ai_message.py
+++ src/ai_message.py
@@ -25,19 +25,20 @@ async def ai_message(message, api = "brainshop", mention = True):
   else:
     lang_current_group = "users"
     lang_current_id = message.author.id
-  resp = ""
-  try:
-    if api in apis:
-      resp = apis[api]["function"](message.content, message.author.id, apis[api]["token"])
-    if get_lang_code(id=lang_current_id, group=lang_current_group) != "en" and get_translate(id=lang_current_id, group=lang_current_group):
-      resp = ts.translate_text(resp, translator= "google", to_language=get_lang_code(id=lang_current_id, group=lang_current_group)) 
-    #for resp in resps['choices']:
-      #await message.channel.send(resp['message']['content'])
-  except Exception as e:
-    print(str(e))
-    resp = "An error occurred please contact with my developer!"
-  resp = resp.replace("<tips> enJoke </tips>", "")
-  if mention:
-    await message.channel.send(f"{message.author.mention}, {resp}")
-  else:
-    await message.channel.send(f"{resp}")
+  async with message.channel.typing():
+    resp = ""
+    try:
+      if api in apis:
+        resp = apis[api]["function"](message.content, message.author.id, apis[api]["token"])
+      if get_lang_code(id=lang_current_id, group=lang_current_group) != "en" and get_translate(id=lang_current_id, group=lang_current_group):
+        resp = ts.translate_text(resp, translator= "google", to_language=get_lang_code(id=lang_current_id, group=lang_current_group)) 
+      #for resp in resps['choices']:
+        #await message.channel.send(resp['message']['content'])
+    except Exception as e:
+      print(str(e))
+      resp = "An error occurred please contact with my developer!"
+    resp = resp.replace("<tips> enJoke </tips>", "")
+    if mention:
+      await message.channel.send(f"{message.author.mention}, {resp}")
+    else:
+      await message.channel.send(f"{resp}")
