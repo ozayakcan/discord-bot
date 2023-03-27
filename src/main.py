4aca326a923792d1d8b8c6a76bf4597f2f7755ff
Özay Akcan <mail@ozayakcan.com.tr>
2023-03-27 22:20:40 +0000

    Fixed keep_alive bug

--- src/main.py
+++ src/main.py
@@ -8,13 +8,13 @@ from os import system
   
 FFMPEG_PATH = os.environ.get("FFMPEG_PATH")
 discord.opus.load_opus("./src/extensions/libopus.so.0.8.0")
+
+bot = MyBot()
+
+keep_alive()
 try:
-  bot = MyBot()
-
-  keep_alive()
-
   bot.run_async()
 except discord.errors.HTTPException:
     print("\n\n\nBLOCKED BY RATE LIMITS\nRESTARTING NOW\n\n\n")
-    system("python3 ./src/extensions/restarter.py")
     system('kill 1')
+    system("python3 ./src/extensions/restarter.py")
