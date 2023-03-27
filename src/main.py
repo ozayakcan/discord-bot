aa1b6085344acc18fccd4b7b7858689458462fb7
Özay Akcan <mail@ozayakcan.com.tr>
2023-03-27 03:05:18 +0000

    Added restarter when discord limit error

--- src/main.py
+++ src/main.py
@@ -4,12 +4,17 @@ import discord
 
 from extensions.keep_alive import keep_alive
 from extensions.bot import MyBot
+from os import system
   
 FFMPEG_PATH = os.environ.get("FFMPEG_PATH")
 discord.opus.load_opus("./src/extensions/libopus.so.0.8.0")
+try:
+  bot = MyBot()
 
-bot = MyBot()
+  keep_alive()
 
-keep_alive()
-
-bot.run_async()
+  bot.run_async()
+except discord.errors.HTTPException:
+    print("\n\n\nBLOCKED BY RATE LIMITS\nRESTARTING NOW\n\n\n")
+    system("python3 ./src/extensions/restarter.py")
+    system('kill 1')
