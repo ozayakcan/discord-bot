8d838e13dd485fd659c924da0c31a8db8f7f9349
ozayakcan <mail@ozayakcan.com.tr>
2023-04-29 10:15:04 +0000

    Add mix not supported error.

--- src/utils/music/_ytdl.py
+++ src/utils/music/_ytdl.py
@@ -11,13 +11,14 @@ from utils import settings
 from ._st import YTDLError, parse_duration
 
 __all__ = (
-    'YTDLSource',
-    'Song',
-    'SongList',
+  'YTDLSource',
+  'Song',
+  'SongList',
 )
 
 # Silence useless bug reports messages
 youtube_dl.utils.bug_reports_message = lambda: ''
+
 
 class YTDLSource(discord.PCMVolumeTransformer):
   YTDL_OPTIONS = {
@@ -37,13 +38,19 @@ class YTDLSource(discord.PCMVolumeTransformer):
   }
 
   FFMPEG_OPTIONS = {
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'before_options':
+    '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
     'options': '-vn',
   }
 
   ytdl = youtube_dl.YoutubeDL(YTDL_OPTIONS)
 
-  def __init__(self, ctx: commands.Context, source: discord.FFmpegPCMAudio, *, data: dict, volume: float = 0.5):
+  def __init__(self,
+               ctx: commands.Context,
+               source: discord.FFmpegPCMAudio,
+               *,
+               data: dict,
+               volume: float = 0.5):
     super().__init__(source, volume)
     settings.update_currents(ctx=ctx)
     self.requester = ctx.author
@@ -69,46 +76,84 @@ class YTDLSource(discord.PCMVolumeTransformer):
     return settings.lang_string("yt_uploader").format(self)
 
   @classmethod
-  async def create_source(cls, ctx: commands.Context, search: str, *, loop: asyncio.BaseEventLoop = None):
+  async def create_source(cls,
+                          ctx: commands.Context,
+                          search: str,
+                          *,
+                          loop: asyncio.BaseEventLoop = None):
+
     loop = loop or asyncio.get_event_loop()
 
     if validators.url(search):
-      partial = functools.partial(cls.ytdl.extract_info, search, download=False, process=False)
+      partial = functools.partial(cls.ytdl.extract_info,
+                                  search,
+                                  download=False,
+                                  process=False)
     else:
-      partial = functools.partial(cls.ytdl.extract_info, f"ytsearch5:{search}", download=False, process=False)
-
+      partial = functools.partial(cls.ytdl.extract_info,
+                                  f"ytsearch5:{search}",
+                                  download=False,
+                                  process=False)
     data = await loop.run_in_executor(None, partial)
     if data is None:
-      raise YTDLError(settings.lang_string("yt_could_not_find").format(search))
-      
+      raise YTDLError(
+        settings.lang_string("yt_could_not_find").format(search))
     webpage_url = data['webpage_url']
-    if validators.url(webpage_url):
-      partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=False)
-      
+    if data["title"].startswith("Mix - "):
+      raise YTDLError(
+          settings.lang_string("mix_not_supported"))
+    elif validators.url(webpage_url):
+      partial = functools.partial(cls.ytdl.extract_info,
+                                  webpage_url,
+                                  download=False)
       processed_info = await loop.run_in_executor(None, partial)
-  
       if processed_info is None:
-        raise YTDLError(settings.lang_string("yt_could_not_fetch").format(webpage_url))
-  
+        raise YTDLError(
+          settings.lang_string("yt_could_not_fetch").format(webpage_url))
+
       sources = []
       if 'entries' not in processed_info:
-        sources.append(Song(cls(ctx, discord.FFmpegPCMAudio(processed_info['url'], **cls.FFMPEG_OPTIONS), data=processed_info)))
+        sources.append(
+          Song(
+            cls(ctx,
+                discord.FFmpegPCMAudio(processed_info['url'],
+                                       **cls.FFMPEG_OPTIONS),
+                data=processed_info)))
       else:
         #entries = list(data["entries"])
         for source in processed_info["entries"]:
-          sources.append(Song(cls(ctx, discord.FFmpegPCMAudio(source['url'], **cls.FFMPEG_OPTIONS), data=source)))
-  
+          sources.append(
+            Song(
+              cls(ctx,
+                  discord.FFmpegPCMAudio(source['url'],
+                                         **cls.FFMPEG_OPTIONS),
+                  data=source)))
+
       if len(sources) == 0:
-        raise YTDLError(settings.lang_string("yt_could_not_fetch").format(search))
-      return SongList(sources, is_playlist=True if processed_info["webpage_url_basename"] == "playlist" else False, playlist_title = processed_info["title"])
+        raise YTDLError(
+          settings.lang_string("yt_could_not_fetch").format(search))
+      return SongList(
+        sources,
+        is_playlist=True
+        if processed_info["webpage_url_basename"] == "playlist" else False,
+        playlist_title=processed_info["title"])
     else:
       s_list = []
       if "entries" in data:
         for sng in data["entries"]:
-          s_list.append({"title":sng["title"], "url":sng["url"], "requester": ctx.author})
+          s_list.append({
+            "title": sng["title"],
+            "url": sng["url"],
+            "requester": ctx.author
+          })
       else:
-        s_list.append({"title":data["title"], "url":data["url"], "requester": ctx.author})
+        s_list.append({
+          "title": data["title"],
+          "url": data["url"],
+          "requester": ctx.author
+        })
       return s_list
+
 
 class Song:
   __slots__ = ('source', 'requester', 'is_playlist', 'playlist_title')
@@ -116,22 +161,32 @@ class Song:
   def __init__(self, source: YTDLSource):
     self.source = source
     self.requester = source.requester
-  
+
   def create_embed(self):
-    embed = (discord.Embed(title=settings.lang_string("now_playing"),
-                           description='```css\n{0.source.title}\n```'.format(self),
-                           color=discord.Color.blurple())
-             .add_field(name=settings.lang_string("duration"), value=self.source.duration)
-             .add_field(name=settings.lang_string("requested_by"), value=self.requester.mention)
-             .add_field(name=settings.lang_string("uploader"), value='[{0.source.uploader}]({0.source.uploader_url})'.format(self))
-             .add_field(name=settings.lang_string("url"), value=settings.lang_string("url_content").format(self))
-             .set_thumbnail(url=self.source.thumbnail))
+    embed = (discord.Embed(
+      title=settings.lang_string("now_playing"),
+      description='```css\n{0.source.title}\n```'.format(self),
+      color=discord.Color.blurple()).add_field(
+        name=settings.lang_string("duration"),
+        value=self.source.duration).add_field(
+          name=settings.lang_string("requested_by"),
+          value=self.requester.mention).add_field(
+            name=settings.lang_string("uploader"),
+            value='[{0.source.uploader}]({0.source.uploader_url})'.format(
+              self)).add_field(
+                name=settings.lang_string("url"),
+                value=settings.lang_string("url_content").format(
+                  self)).set_thumbnail(url=self.source.thumbnail))
 
     return embed
 
+
 class SongList:
 
-  def __init__(self, sources : list[Song], is_playlist: bool = False, playlist_title: str = ""):
+  def __init__(self,
+               sources: list[Song],
+               is_playlist: bool = False,
+               playlist_title: str = ""):
     self.sources = sources
     self.is_playlist = is_playlist
     self.playlist_title = playlist_title
